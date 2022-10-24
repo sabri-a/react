1e1fa059e341ed65f328041008f3fa3a6ac805c0
sabri-a <sabribenmohamedfawzi@gmail.com>
2022-10-24 17:37:34 +0100

    react

---
+++ src/App.js
@@ -0,0 +1,46 @@
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Container from 'react-bootstrap/Container';
+import Navbar from 'react-bootstrap/Navbar'
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar>
+        <Container>
+          <Navbar.Brand href="#home">First react project</Navbar.Brand>
+          <Navbar.Toggle />
+          <Navbar.Collapse className="justify-content-end">
+            <Navbar.Text>
+              <a href="#login"> Sabri Ahmed</a>
+            </Navbar.Text>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+      <Form>
+        <Form.Group className="mb-3" controlId="formBasicEmail" >
+          <Form.Label>Email address</Form.Label>
+          <Form.Control type="email" placeholder="Enter email" />
+          <Form.Text className="text-muted">
+            We'll never share your email with anyone else.
+          </Form.Text>
+        </Form.Group>
+        <Form.Group className="mb-3" controlId="formBasicPassword">
+          <Form.Label>Password</Form.Label>
+          <Form.Control type="password" placeholder="Password" />
+        </Form.Group>
+        <Form.Group className="mb-3" controlId="formBasicCheckbox">
+          <Form.Check type="checkbox" label="Check me out" />
+        </Form.Group>
+        <Button variant="primary" type="submit">
+          Submit
+        </Button>
+      </Form>
+      <img src='https://globalgovernanceforum.org/wp-content/uploads/2020/10/Albert-Einstein.jpg' width='100px' />
+    </div>
+  );
+}
+
+export default App;
